386eb577f88649fbf42a3fbb9fbc6c65ae2fd37e
unknown <88levanhung@gmail.com>
2021-06-04 17:28:46 +0700

    d

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ let weather={
         // const {speed}=data.wind ;
         
         document.querySelector(".city").innerText="Weather in "+ name ;
-        document.querySelector('.temp').innerText=temp+" °C";
+        document.querySelector('.temp').innerText=temp*0.1 +" °C";
         document.querySelector('.icon').src="https://openweathermap.org/img/wn/"+ icon +"@2x.png";
         document.querySelector('.description').innerText=description;
         document.querySelector('.humidity').innerText="humidity: "+humidity+" °C";
